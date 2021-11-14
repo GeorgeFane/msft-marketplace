2cc13df4d260cf93b354fb4e982b190f782a68e5
michaelmat3 <89110252+michaelmat3@users.noreply.github.com>
2021-11-14 18:00:33 -0500

    Create Royalty.sol

---
+++ src/contract/Royalty.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.0;
+
+contract Royalty {
+    address public admin; //admin adress, maybe implement Ownable from openzepplin 
+    uint256 mktRoyalty;//royalties for market place 
+    uint256 devRoyalty; //royalties for Game Dev
+    uint256 creatorRoyalty; //royalties for Creator/artist
+    
+    constructor (IERC1155 game) public{
+        _game = game;
+        admin = msg.sender;
+    }
+    //@Dev set royalty percentages
+     function setMktRoyalty(
+        uint256 royaltyPercent
+        ) public{
+            require(msg.sender == admin, "Only Admin can create token types");
+            mktRoyalty = royaltyPercent;
+        }   
+         function setDevRoyalty(
+         uint256 royaltyPercent
+         ) public{
+             require(msg.sender == admin, "Only Admin can create token types");
+             devRoyalty = royaltyPercent;
+         } 
+         
+    function setCreatorRoyalty(
+        uint256 royaltyPercent
+        ) public{
+            require(msg.sender == admin, "Only Admin can create token types");
+            creatorRoyalty = royaltyPercent;
+        }
+        //sendRoyalties for Ether transactions 
+        function sendRoyalties(
+        address seller,
+        address buyer,
+        address mktplace, 
+        address gamedev,
+        address creator,
+        uint256 amount //total amount of transction in ether internal
+        ) internal{
+            uint256 remainingPercent = 100-(mktRoyalty+devRoyalty+creatorRoyalty);
+            uint256 remainder = amount*(remainingPercent/100);
+            buyer.transfer(amount);
+            gamedev.send(amount*(devRoyalty/100));
+            mktplace.send(amount*(mktRoyalty/100));
+            creator.send(amount*(creatorRoyalty/100));
+            seller.transfer(remainder);
+           
+        }
+}
